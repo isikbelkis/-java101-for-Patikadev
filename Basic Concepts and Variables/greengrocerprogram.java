f1da968ca63bf0ab749c47622e0b09f0be4acb07
Belkıs Işık <129294955+isikbelkis@users.noreply.github.com>
2023-06-06 19:37:12 +0300

    Add files via upload

---
+++ Basic Concepts and Variables/greengrocerprogram.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class greengrocerprogram {
+    Scanner scan=new Scanner(System.in);
+    int kilo;
+        //armutkilo=2.14, elmakilo=3.67, muzkilo=0.95, patlıcankilo=5,domateskilo=1.11
+        int armutkilo, elmakilo, muzkilo, patlicankilo,domateskilo;
+        double toplam;
+        
+        System.out.println("Elma kaç kilo olacak:");
+        elmakilo=scan.nextInt();
+        
+        System.out.println("Armut kaç kilo olacak:");
+        armutkilo=scan.nextInt();
+        
+        System.out.println("Muz kaç kilo olacak:");
+        muzkilo=scan.nextInt();
+        
+        System.out.println("Patlıcan kaç kilo olacak:");
+        patlicankilo=scan.nextInt();
+        
+        System.out.println("Domates kaç kilo olacak:");
+        domateskilo=scan.nextInt();
+        
+        toplam=(domateskilo*1.11)+(elmakilo*3.67)+(armutkilo*2.14)+(muzkilo*0.95)+(patlicankilo*5);
+        System.out.println("Toplam Tutar:"+toplam);
+    
+}
